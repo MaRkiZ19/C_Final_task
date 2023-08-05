f9e7005d0153fab6b2ae4ac2d863dc8b577dc75f
ваше_имя <grigorenko-marko@rambler.ru>
2023-08-05 21:09:35 +0300

    programm v2 OK

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿
-string[] array1 = new string[] {"Hello", "2", "world", ":-)"};
-string[] array2 = new string[array1.Length];
+int ArrayLenght()
+{
+    System.Console.Write("Enter the desired number of lines: ");
+    int len = Convert.ToInt32(Console.ReadLine());
+    return len;
+}
+
+void FillingArray(int len, string[] array1)
+{
+    for (int i = 0; i < len; i++)
+            {
+                Console.Write("Enter the string №{0}:\r\n    ", i + 1);
+                array1[i] = Console.ReadLine(); 
+            }
+}
 
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
@@ -24,5 +37,11 @@ void PrintArray(string[] array)
     Console.WriteLine();
 }
 
+int len = ArrayLenght();
+// string[] array1 = new string[] {"Hello", "2", "world", ":-)"};
+string[] array1 = new string[len];
+string[] array2 = new string[array1.Length];
+FillingArray(len, array1);
+PrintArray(array1);
 SecondArrayWithIF(array1, array2);
 PrintArray(array2);
